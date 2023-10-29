6f4b9fa14072e88cf8596b9ea2187ca2ff061b99
Michal S <minitrikus@gmail.com>
2023-10-29 19:46:40 +0100

    files creation, js variables, functions add, subtract, divide, multiply

---
+++ calculator.js
@@ -0,0 +1,9 @@
+let  num1;
+let num2;
+let operator;
+
+let add = (num1, num2) => {return num1 + num2};
+let subtract = (num1, num2) => {return num1 - num2};
+let multiply = (num1, num2) => {return num1 * num2};
+let divide = (num1, num2) => {return num1 / num2};
+
