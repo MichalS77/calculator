e9a173362fc01d796c5ccfb744b5074cb3f69079
Michal S <minitrikus@gmail.com>
2023-10-31 14:50:50 +0100

    edit: add function, html, operate

--- calculator.js
+++ calculator.js
@@ -1,15 +1,48 @@
-let  num1;
+let num1;
 let num2;
 let operator;
+let btnAdd = document.querySelector('#btnAdd');
+let btnEqual = document.querySelector('#btnEqual');
 
-let add = (num1, num2) => {return num1 + num2};
+document.addEventListener("DOMContentLoaded", () =>{
+    let buttonValue = document.querySelectorAll('.btnValue');
+    let display = document.getElementById('display');
+    
+console.log(buttonValue);
+
+    buttonValue.forEach(button => {        
+        button.addEventListener('click', () => {
+            console.log("button clicked");
+            console.log(button.value)
+            display.innerText += button.value;
+        })
+    });
+        
+    btnAdd.addEventListener('click', () => {
+        console.log('button clicked');
+        add()
+    }); 
+    
+    btnEqual.addEventListener('click', () => {
+        num2 = display.innerText;
+        display.textContent = operate(num1, num2, operator);
+    })
+    
+})
+let add = () => {
+    num1=display.innerText;
+    display.innerText = '';
+    num2='';
+    operator = '+';
+    };
 let subtract = (num1, num2) => {return num1 - num2};
 let multiply = (num1, num2) => {return num1 * num2};
 let divide = (num1, num2) => {return num1 / num2};
 
 let operate = (num1, num2, operator) => {
     if (operator === '+'){
-        return add(num1, num2);
+        num2 = display.innerText;
+        return parseInt(num1) + parseInt(num2);
     }else if (operator === '-'){
         return subtract(num1, num2);
     }else if (operator === '/'){
@@ -18,6 +51,10 @@ let operate = (num1, num2, operator) => {
         return multiply(num1, num2);
     }
 }
+
+
+
+
 
 
 
