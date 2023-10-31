951a343ff21ff033ae53a40964ce922373bcf19c
Michal S <minitrikus@gmail.com>
2023-10-31 17:25:18 +0100

    operation function

--- calculator.js
+++ calculator.js
@@ -2,51 +2,50 @@ let num1;
 let num2;
 let operator;
 let btnAdd = document.querySelector('#btnAdd');
-let btnEqual = document.querySelector('#btnEqual');
+
 
 document.addEventListener("DOMContentLoaded", () =>{
-    let buttonValue = document.querySelectorAll('.btnValue');
+    let btnValue = document.querySelectorAll('.btnValue');
+    let btnOperation = document.querySelectorAll('.btnOperation');
     let display = document.getElementById('display');
     
-console.log(buttonValue);
+console.log(btnValue);
 
-    buttonValue.forEach(button => {        
+    btnValue.forEach(button => {        
         button.addEventListener('click', () => {
             console.log("button clicked");
             console.log(button.value)
             display.innerText += button.value;
         })
     });
-        
-    btnAdd.addEventListener('click', () => {
-        console.log('button clicked');
-        add()
-    }); 
-    
+
     btnEqual.addEventListener('click', () => {
         num2 = display.innerText;
         display.textContent = operate(num1, num2, operator);
-    })
-    
-})
-let add = () => {
-    num1=display.innerText;
-    display.innerText = '';
-    num2='';
-    operator = '+';
-    };
-let subtract = (num1, num2) => {return num1 - num2};
+    });
+
+    btnOperation.forEach(button => {
+        button.addEventListener('click', () =>{
+        num1=display.innerText;
+        display.innerText = '';
+        num2='';
+        operator = button.value;
+        })
+    });
+});
+
 let multiply = (num1, num2) => {return num1 * num2};
 let divide = (num1, num2) => {return num1 / num2};
 
 let operate = (num1, num2, operator) => {
+    num2 = parseInt(display.innerText);
+
     if (operator === '+'){
-        num2 = display.innerText;
         return parseInt(num1) + parseInt(num2);
     }else if (operator === '-'){
-        return subtract(num1, num2);
+      return parseInt(num1) - parseInt(num2);
     }else if (operator === '/'){
-        return divide(num1, num2);
+        return num1 / num2;
     }else if(operator === '*'){
         return multiply(num1, num2);
     }
